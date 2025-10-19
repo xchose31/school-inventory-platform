73b19f7a3c5b6cc1b72b2dd653547b26c13e411f
xchose31 <rte3001@vk.com>
2025-10-19 23:21:54 +0300

    login function added

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,24 @@
-from app import app
-from flask import render_template
+from app import app, db
+from flask import render_template, flash, redirect, url_for
+from flask_login import login_user, logout_user, current_user, login_required
+from app.forms import LoginForm
+import sqlalchemy as sa
+from app.models import User
 
 @app.route('/')
 def index():
     return render_template('base.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data)
+        )
+        if user is not None or User.password != form.password.data:
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
